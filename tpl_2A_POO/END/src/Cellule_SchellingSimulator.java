a7a233b53d2d6f1619b81a759cd3638633274c45
Filipe Gomes <fgomes@gomesfilipe-pc.home>
2015-11-16 18:39:09 +0100

    cellules javadoc

--- tpl_2A_POO/END/src/Cellule_SchellingSimulator.java
+++ tpl_2A_POO/END/src/Cellule_SchellingSimulator.java
@@ -3,55 +3,50 @@ import gui.GUISimulator;
 import gui.Rectangle;
 import java.awt.Color;
 
-public class Cellule_SchellingSimulator extends CelluleSimulator {
+public class Cellule_SchellingSimulator extends CelluleSimulator implements Simulable {
 
     private Cellule_Schelling cells_schelling;
 
-    /* Constructeur pour la simulation graphique */
+    /** Constructeur pour la simulation graphique */
     public Cellule_SchellingSimulator(int taillex, int tailley, int n, int k) {
-	super(taillex,tailley,n);
-	this.cells_schelling = new Cellule_Schelling(taillex,tailley,n,k);
+		super(taillex,tailley,n);
+		this.cells_schelling = new Cellule_Schelling(taillex,tailley,n,k);
     }
 	
-    /* On récupère la grillle */
+	/** On récupère la grillle */
     public Cellule_Schelling getCells() {
-	return this.cells_schelling;
+		return this.cells_schelling;
     }
 
-    /* fonction d'affichage pour l'interface grahique 
+    /** fonction d'affichage pour l'interface grahique 
     // On 'colorie' la grille suivant la couleur des maisons */
     public void Affiche() {
 	
-	this.getGUI().reset();
+		this.getGUI().reset();
 	
-	for (int i = 0; i < this.cells_schelling.getLignes(); i++) {
-	    for (int j = 0; j < this.cells_schelling.getColonnes(); j++) {
-		int etatk = this.cells_schelling.getEtatav(i,j);
-		//chaque maison est repéré par un entier,
-		//définissant une couleur unique
-		Color coul = new Color (255, 255, 255);
-		//si la maison est vide (etatk=0), coul reste inchangé (blanc)
-		if (etatk != 0) {
-		    coul = new Color ((40*etatk) % 255,
-				      (255-40*etatk) % 255,
-				      (255-40*etatk) % 255);
-		} 
-		this.getGUI().addGraphicalElement(new Rectangle(10*i,10*j,
-								coul,coul,
-								10,10));
-	    }
-	}
+		for (int i = 0; i < this.cells_schelling.getLignes(); i++) {
+	    	for (int j = 0; j < this.cells_schelling.getColonnes(); j++) {
+				int etatk = this.cells_schelling.getEtatav(i,j);
+				//chaque maison est repéré par un entier, définissant une couleur unique
+				Color coul = new Color (255, 255, 255);
+				//si la maison est vide (etatk=0) alors coul reste inchangé (blanc)
+				if (etatk != 0) {
+					coul = new Color ((40*etatk) % 255,(255-40*etatk) % 255,(255-40*etatk) % 255);
+				} 
+				this.getGUI().addGraphicalElement(new Rectangle(10*i,10*j,coul,coul,10,10));
+	    	}
+		}
     }
 
-    /* Implémentation du bouton next, on passe du temps t au temps t+1, 
+	/** Implémentation du bouton next, on passe du temps t au temps t+1, 
     // pûis on affiche */
     @Override
-    public void next() {
-	this.cells_schelling.evolution();
-	this.Affiche();
-    }
+     public void next() {
+	 	this.cells_schelling.evolution();
+	 	this.Affiche();
+     }
  
-    /* Implémentation du bouton restart, on revient au temps 0 
+ 	/** Implémentation du bouton restart, on revient au temps 0 
     // pûis on affiche */
     @Override
     public void restart() {
